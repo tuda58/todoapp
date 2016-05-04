bcf99f8dafc6f5020af90e2568563aaa3e6fa581
Do Tu <dotu@Dos-MacBook-Pro-2.local>
2016-05-04 14:07:24 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    remoteUrl : 'mongodb://tuda_todoapp:123456789@jello.modulusmongo.net:27017/ah7Isiru',
+    localUrl: 'mongodb://tuda_todoapp:123456789@jello.modulusmongo.net:27017/ah7Isiru'
+};
